6f38d96b33add46061bb121455e9ed56c0b639ed
pranmandale <pranavmandale2025@gmail.com>
2024-06-29 22:20:39 +0530

    new files added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage: 'url(https://images.pexels.com/photos/259165/pexels-photo-259165.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)'}}
+    >
+
+    </div>
+  )
+}
+
+export default App
